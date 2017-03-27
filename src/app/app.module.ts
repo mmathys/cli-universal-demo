51e73ae545551e4fc6c25b62bb69039c57713ea8
Everton R. Auler <evertonrobertoauler@gmail.com>
2017-03-27 10:20:06 -0300

    Adding @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    MaterialModule.forRoot(),
     BrowserModule.withServerTransition({ appId: 'cli-universal-demo' }),
     RouterModule.forRoot([
       { path: '', loadChildren: './home/home.module#HomeModule' },
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    MaterialModule,
     RouterModule.forChild([
       { path: '', component: HomePageComponent, pathMatch: 'full' }
     ])
