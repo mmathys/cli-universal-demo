a8ac8597be8e84fa3ed9b36824e5e852c4a2a060
Everton R. Auler <evertonrobertoauler@gmail.com>
2017-04-13 15:51:04 -0300

    Adding http example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  post: Observable<any>;
+
+  constructor(private _http: Http) { }
+
+  ngOnInit() {
+    this.post = this._http.get('https://jsonplaceholder.typicode.com/posts/1')
+      .map(res => res.json());
+  }
 }
