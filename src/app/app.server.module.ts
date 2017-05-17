63ab29496110994ba432af77d4e89cb5271c4be4
Max Mathys <max.mathys@swisscom.com>
2017-05-17 12:45:42 +0200

    sorry, fix

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -4,14 +4,6 @@ import { NgModule, NgModuleFactory, NgModuleFactoryLoader } from '@angular/core'
 import { ServerModule } from '@angular/platform-server';
 import { AppModule, AppComponent } from './app.module';
 
-export function getRequest() {
-  const req = Zone.current.get('req') || {};
-  return req;
-}
-export function getResponse() {
-  return Zone.current.get('res') || {};
-}
-
 export class ServerFactoryLoader extends NgModuleFactoryLoader {
   load(path: string): Promise<NgModuleFactory<any>> {
     return new Promise((resolve, reject) => {
@@ -29,9 +21,7 @@ export class ServerFactoryLoader extends NgModuleFactoryLoader {
   ],
   bootstrap: [AppComponent],
   providers: [
-    { provide: NgModuleFactoryLoader, useClass: ServerFactoryLoader },
-    { provide: 'req', useFactory: getRequest },
-    { provide: 'res', useFactory: getResponse },
+    { provide: NgModuleFactoryLoader, useClass: ServerFactoryLoader }
   ]
 })
 export class AppServerModule { }
